4f51fb9a46804062c9050f8aa2377d01df77c9a4
Abhishek Kumar <abhi9075@gmail.com>
2024-06-12 20:22:55 +0530

    changes new

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "my-first-gcp-ci-cd-check" {
   name          = "gcp-ci-cd-first-check"
-  project       = "mfuture-glider-422215-t3"
+  project       = "future-glider-422215-t3"
   location      = "US"
   force_destroy = true
 
