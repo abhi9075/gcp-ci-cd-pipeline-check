b1559b51f40a9ea2251456078ee6c34d5f3030c7
Abhishek Kumar <abhi9075@gmail.com>
2024-06-12 20:03:58 +0530

    new changes

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "google_storage_bucket" "my-first-gcp-ci-cd-check" {
   name          = "no-public-access-bucket"
+  project       = "gcp-ci-cd-first"
   location      = "US"
   force_destroy = true
 
