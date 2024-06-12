75592b0ce5a7408b2847ef6c34da01019a0c8420
Abhishek Kumar <abhi9075@gmail.com>
2024-06-12 20:18:26 +0530

    changes in yml

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "my-first-gcp-ci-cd-check" {
   name          = "gcp-ci-cd-first-check"
-  project       = "my_git_service"
+  project       = "mfuture-glider-422215-t3"
   location      = "US"
   force_destroy = true
 
