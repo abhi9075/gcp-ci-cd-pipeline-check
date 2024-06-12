4ae711379979f9b6760b8db91f51eda1ee4c6e93
Abhishek Kumar <abhi9075@gmail.com>
2024-06-12 20:14:14 +0530

    changes

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "my-first-gcp-ci-cd-check" {
-  name          = "gcp-ci-cd-first"
-  project       = "gcp-ci-cd-first"
+  name          = "gcp-ci-cd-first-check"
+  project       = "my_git_service"
   location      = "US"
   force_destroy = true
 
